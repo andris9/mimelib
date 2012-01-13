1d760b0266d31db030369b2e842a99c4c83cb97a
Andris Reinman <andris@node.ee>
2012-01-13 14:17:37 +0200

    Added reversed Content Types list

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 
 module.exports = require("./mime-functions");
 module.exports.contentTypes = require("./content-types");
+module.exports.contentTypesReversed = require("./content-types-reversed");
